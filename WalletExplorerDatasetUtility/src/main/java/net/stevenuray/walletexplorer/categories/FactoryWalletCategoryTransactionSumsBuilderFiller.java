6519f13f4883dea8a37b4ca6b2999e1cc06db65a
stevenuray <Steven.Uray@gmail.com>
2016-01-15 18:57:59 -0600

    Adding two classes missed in the last update to CategoryReviewer and it's components.

---
+++ WalletExplorerDatasetUtility/src/main/java/net/stevenuray/walletexplorer/categories/FactoryWalletCategoryTransactionSumsBuilderFiller.java
@@ -0,0 +1,43 @@
+package net.stevenuray.walletexplorer.categories;
+
+import java.util.Iterator;
+import org.joda.time.Interval;
+import net.stevenuray.walletexplorer.persistence.timable.TimableDataProducer;
+import net.stevenuray.walletexplorer.persistence.timable.TimableWalletNameDataProducerFactory;
+import net.stevenuray.walletexplorer.walletattribute.dto.ConvertedWalletTransaction;
+
+public class FactoryWalletCategoryTransactionSumsBuilderFiller implements
+		TransactionSumsBuilderFiller {
+	private final TimableWalletNameDataProducerFactory<ConvertedWalletTransaction> producerFactory;
+	private final WalletCategoryTransactionSumsBuilder builder;
+	private final Iterator<String> walletNames;
+	private final Interval timespan;
+	
+	public FactoryWalletCategoryTransactionSumsBuilderFiller(
+			TimableWalletNameDataProducerFactory<ConvertedWalletTransaction> producerFactory,
+			Iterator<String> walletNames,Interval timespan,WalletCategoryTransactionSumsBuilder builder){
+		this.producerFactory = producerFactory;
+		this.walletNames = walletNames;
+		this.timespan = timespan;
+		this.builder = builder;
+	}
+
+	public WalletCategoryTransactionSums getSums() {
+		while(walletNames.hasNext()){
+			String nextWalletName = walletNames.next();
+			addTransactionsToBuilder(nextWalletName);
+		}
+		WalletCategoryTransactionSums sums = builder.build();
+		return sums;
+	}
+
+	private void addTransactionsToBuilder(String nextWalletName) {
+		TimableDataProducer<ConvertedWalletTransaction> producer = 
+				producerFactory.getDataProducer(nextWalletName, timespan);
+		Iterator<ConvertedWalletTransaction> producerIterator = producer.getData();
+		while(producerIterator.hasNext()){
+			ConvertedWalletTransaction nextProduct = producerIterator.next();
+			builder.insert(nextProduct);
+		}		
+	}
+}
---
+++ WalletExplorerDatasetUtility/src/main/java/net/stevenuray/walletexplorer/categories/TransactionSumsBuilderFiller.java
@@ -0,0 +1,7 @@
+package net.stevenuray.walletexplorer.categories;
+
+public interface TransactionSumsBuilderFiller {
+
+	public abstract WalletCategoryTransactionSums getSums();
+
+}
