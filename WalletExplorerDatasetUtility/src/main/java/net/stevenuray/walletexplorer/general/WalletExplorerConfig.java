f783a91f0e3e67200fda0468cb0afa2ce2d71fef
stevenuray <Steven.Uray@gmail.com>
2016-02-04 21:47:51 -0600

    Reducing maximum queue size in an attempt to prevent out of memory errors on machines with low memory.

--- WalletExplorerDatasetUtility/src/main/java/net/stevenuray/walletexplorer/general/WalletExplorerConfig.java
+++ WalletExplorerDatasetUtility/src/main/java/net/stevenuray/walletexplorer/general/WalletExplorerConfig.java
@@ -5,7 +5,7 @@ import org.joda.time.Interval;
 
 //TODO better configuration methods!
 public class WalletExplorerConfig {
-	public static final int MAX_QUEUE_LENGTH = 1000;
+	public static final int MAX_QUEUE_LENGTH = 100;
 	public static final int MAX_THREADS = 5;
 	
 	/**Assumes no bitcoin transactions could have been generated before the genesis block. */
